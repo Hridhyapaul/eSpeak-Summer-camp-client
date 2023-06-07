448628f002b0d6781e921795bed537e955e6950e
Hridhyapaul <hpdesign2021@gmail.com>
2023-06-07 16:26:24 +0600

    Created Footer component

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../Pages/Home/Shared/Navigation";
+import Footer from "../Pages/Home/Shared/Footer";
 
 const Main = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
